d5288501b4804a211f84e55f4a690fce83831aac
Ramon Aseniero <raseniero@gmail.com>
2023-05-20 17:47:14 -0700

    added static url

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),  # new
     path("", include("snippets.urls")),  # new
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
